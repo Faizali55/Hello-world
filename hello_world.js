2407027a918f27ccc50ddce72cbd066358784b41
Faiz Ali <164006663+Faizali55@users.noreply.github.com>
2024-03-20 14:55:56 +0500

    Add files via upload

---
+++ hello_world.js
@@ -0,0 +1,6 @@
+"use strict";
+//WRTITE HELLO WORLD SIMPLE METHOD.
+console.log("Hello world");
+//WRTITE HELLO WORLD logical METHOD.
+let variable = "Hello World";
+console.log(variable);
---
+++ hello_world.ts
@@ -0,0 +1,6 @@
+//WRTITE HELLO WORLD SIMPLE METHOD.
+console.log("Hello world");
+
+//WRTITE HELLO WORLD logical METHOD.
+let variable: string = "Hello World";
+console.log(variable);
